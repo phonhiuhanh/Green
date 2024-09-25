4cd1854bff658f15ef8b69e01a13b53b9da50309
thanhnghia <thanhnghia1702dz@gmail.com>
2024-09-25 21:55:35 +0700

    giao diện admin: khách hàng, nhân viên, đơn hàng.

--- src/main/java/com/poly/greeen/Controller.java
+++ src/main/java/com/poly/greeen/Controller.java
@@ -9,4 +9,25 @@ public class Controller {
     public String index(Model model) {
         return "admin/index";
     }
+
+    @GetMapping("/nhanvien")
+    public String employee(Model model) {
+        model.addAttribute("message", "Chào mừng đến trang nhân viên!");
+        return "/QLnhanvien/nhanvien"; // Tên file HTML của trang nhân viên
+    }
+
+    @GetMapping("/khachhang")
+    public String customer(Model model) {
+        model.addAttribute("message", "Chào mừng đến trang khách hàng!");
+        return "QLkhachhang/khachhang"; // Tên file HTML của trang khách hàng
+    }
+
+    @GetMapping("/donhang")
+    public String order(Model model) {
+        model.addAttribute("message", "Chào mừng đến trang đơn hàng!");
+        return "QLdonhang/donhang"; // Tên file HTML của trang đơn hàng
+    }
 }
+
+
+
